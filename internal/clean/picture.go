160efd61c01cb3a6c5c34dff4678d31f6f64037f
akeil <alex@akeil.net>
2021-02-13 21:38:02 +0100

    Resolve picture srcset in elements
    Basic functionality working, not much in the way of handling bad data
    or errors.
    
    Error handling is basically "give up" and leave the HTML unchanged.

---
+++ internal/clean/picture.go
@@ -0,0 +1,136 @@
+package clean
+
+import (
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func resolvePicture(doc *goquery.Document) {
+	doc.Selection.Find("picture").Each(func(i int, s *goquery.Selection) {
+		img := s.Find("img")
+		if img.Length() != 1 {
+			log.Println("Found multiple img elements in <picture>")
+			return
+		}
+        log.Println(s.Html())
+
+		sources := make([]source, 0)
+		s.Find("source").Each(func(j int, source *goquery.Selection) {
+			parsed := parseSource(source)
+			if len(parsed.srcset) != 0 {
+				sources = append(sources, parsed)
+			}
+		})
+
+		srcset := selectSrcset(sources)
+		if srcset.url == "" {
+			log.Println("no suitable src found for <picture>")
+			return
+		}
+
+		img.SetAttr("src", srcset.url)
+		s.Find("source").Remove()
+		s.Contents().Unwrap()
+
+	})
+}
+
+func parseSource(s *goquery.Selection) source {
+	typ, _ := s.Attr("type")
+	media, _ := s.Attr("media")
+	srcs, _ := s.Attr("srcset")
+
+	srcsets := make([]srcset, 0)
+	options := strings.Split(srcs, ",")
+
+	for _, o := range options {
+		o = strings.TrimSpace(o)
+		parts := strings.Split(o, " ")
+		if len(parts) == 0 {
+			return source{}
+		} else if len(parts) > 2 {
+			log.Printf("invalid srcset: %q", o)
+			return source{}
+		}
+
+		url := parts[0]
+		srcset := srcset{url: url}
+
+		if len(parts) == 2 {
+			x := parts[1]
+
+			if strings.HasSuffix(x, "w") {
+				width, err := strconv.ParseUint(strings.TrimSuffix(x, "w"), 10, 64)
+				if err != nil {
+					log.Printf("failed to parse int from %q", x)
+					return source{}
+				}
+				srcset.width = width
+
+			} else if strings.HasSuffix(x, "x") {
+				density, err := strconv.ParseFloat(strings.TrimSuffix(x, "x"), 64)
+				if err != nil {
+					log.Printf("failed to parse float from %q", x)
+					return source{}
+				}
+				srcset.density = density
+			}
+
+		}
+		srcsets = append(srcsets, srcset)
+	}
+
+	return source{
+		contentType: typ,
+		media:       media,
+		srcset:      srcsets,
+	}
+}
+
+func selectSrcset(sources []source) srcset {
+	if len(sources) == 0 {
+		return srcset{}
+	}
+
+	var maxWidth uint64
+	var maxDensity float64
+	var byWidth srcset
+	var byDensity srcset
+
+	for _, source := range sources {
+		for _, srcset := range source.srcset {
+			if srcset.width != 0 && srcset.width > maxWidth {
+				maxWidth = srcset.width
+				byWidth = srcset
+			} else if srcset.density != 0 && srcset.density > maxDensity {
+				maxDensity = srcset.density
+				byDensity = srcset
+			}
+		}
+	}
+
+	if maxWidth != 0 {
+		return byWidth
+	} else if maxDensity != 0 {
+		return byDensity
+	}
+
+	// falback, simply return the last <source> entry, first url
+	s := sources[len(sources)-1].srcset
+	return s[0]
+}
+
+type source struct {
+	contentType string
+	media       string
+	srcset      []srcset
+}
+
+type srcset struct {
+	url     string
+	width   uint64
+	density float64
+}
---
+++ internal/clean/picture_test.go
@@ -0,0 +1,27 @@
+package clean
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPicture(t *testing.T) {
+	assert := assert.New(t)
+
+	html := "<picture><img src=\"default.jpg\"/></picture>"
+	d := doc(html)
+	resolvePicture(d)
+	assert.Equal(html, str(d))
+
+	html = `<picture>
+        <source srcset="large.png 200w, small.png 100w" />
+        <source srcset="dense.png 2x, standard.png 1x" />
+        <source srcset="" />
+        <img src="default.jpg" />
+    </picture>`
+	d = doc(html)
+	resolvePicture(d)
+	assert.Equal("<img src=\"large.png\"/>", strings.TrimSpace(str(d)))
+}
