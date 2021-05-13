42c1e75e639d4510d15c9e603585f3ed4100976d
akeil <alex@akeil.net>
2021-05-13 09:44:40 +0200

    Move "prepare" to separate file

--- internal/content/clean_test.go
+++ internal/content/clean_test.go
@@ -8,15 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUnwrapNoscript(t *testing.T) {
-	assert := assert.New(t)
-
-	d := doc("<p>foo <noscript><em>bar</em></noscript> baz</p>")
-	unwrapNoscript(d)
-	assert.Equal("<p>foo <em>bar</em> baz</p>", str(d))
-
-}
-
 func TestUnwrap(t *testing.T) {
 	assert := assert.New(t)
 
