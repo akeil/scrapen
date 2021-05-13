daebde35aa27ec4dcc3befe91b5697ad3a5bf716
akeil <alex@akeil.net>
2021-05-13 10:02:33 +0200

    Apply readbility to all versions, select best

--- internal/readable/readable.go
+++ internal/readable/readable.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	readability "github.com/go-shiori/go-readability"
 	log "github.com/sirupsen/logrus"
 
@@ -11,23 +12,84 @@ import (
 )
 
 func MakeReadable(ctx context.Context, t *pipeline.Task) error {
-
 	log.WithFields(log.Fields{
 		"task":   t.ID,
 		"module": "readable",
 		"url":    t.ContentURL(),
 	}).Info("Apply readability")
 
-	p := readability.NewParser()
-	r := strings.NewReader(t.HTML())
+	baseURL := t.ContentURL()
+	candidates := make([]readability.Article, 0)
 
-	a, err := p.Parse(r, t.ContentURL())
+	a, err := doReadability(t.Document(), baseURL)
 	if err != nil {
 		return err
 	}
+	candidates = append(candidates, a)
 
-	t.SetHTML(a.Content)
-	t.Title = a.Title
+	altDoc := t.AltDocument()
+	if altDoc != nil {
+		altA, err := doReadability(altDoc, baseURL)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"task":   t.ID,
+				"module": "readable",
+				"url":    t.ContentURL(),
+				"error":  err,
+			}).Warning("Readability failed for alternate content")
+		} else {
+			candidates = append(candidates, altA)
+		}
+	}
+
+	article := selectArticle(candidates)
+
+	t.SetHTML(article.Content)
+	t.Title = article.Title
 
 	return nil
+}
+
+func doReadability(doc *goquery.Document, baseURL string) (readability.Article, error) {
+	var a readability.Article
+	p := readability.NewParser()
+	s, err := doc.Selection.Find("html").First().Html()
+	if err != nil {
+		return a, err
+	}
+
+	r := strings.NewReader(s)
+
+	a, err = p.Parse(r, baseURL)
+	if err != nil {
+		return a, err
+	}
+
+	return a, nil
+}
+
+func selectArticle(candidates []readability.Article) readability.Article {
+	var result readability.Article
+	maxlen := -1
+
+	for _, a := range candidates {
+		r := strings.NewReader(a.Content)
+		doc, err := goquery.NewDocumentFromReader(r)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"module": "readable",
+				"error":  err,
+			}).Warning("Failed to parse Document from content.")
+			continue
+		}
+		// count words
+		text := doc.Selection.Find("body").First().Text()
+		l := len(text)
+		if l > maxlen {
+			maxlen = l
+			result = a
+		}
+	}
+
+	return result
 }
